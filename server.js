5e9c2f798e92aa43ff9b4b83d302e46d223681df
Rajavel Kamaraj <jkamarr@jci.com>
2022-04-12 23:25:31 +0530

    cc

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000;
-app.use(requireHTTPS);
-app.use(express.static(__dirname+ "./dist/neohuma"));
+//app.use(requireHTTPS);
+app.use(express.static(__dirname+ '/dist/neohuma'));
 
 
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + "/dist/neohuma/index.html")
+    res.sendFile(path.join(__dirname + '/dist/neohuma/index.html')
   );
   });
 
@@ -17,11 +17,11 @@ app.listen(port, () => {
     console.log(`app running on ${port}`);
   });
 
-  function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
+//   function requireHTTPS(req, res, next) {
+//     // The 'x-forwarded-proto' check is for Heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
 }
 
